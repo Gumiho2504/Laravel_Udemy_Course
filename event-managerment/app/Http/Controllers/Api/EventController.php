7ee78de941d9ffd63e02cccfaeb07b5cffc64cc3
Gumiho2504 <hemchanmetrey123@gmail.com>
2024-12-24 16:09:38 +0700

    api resource

--- event-managerment/app/Http/Controllers/Api/EventController.php
+++ event-managerment/app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,8 +14,8 @@ class EventController extends Controller
      */
     public function index()
     {
-        $events = Event::all();
-        return $events;
+        //$events = Event::all();
+        return EventResource::collection(Event::with('user','attendees')->get());
     }
 
     /**
@@ -22,7 +23,19 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $event = Event::create(
+            [
+                ...$request->validate([
+                    'name' => 'required|string|max:255',
+                    'description' => 'nullable|string',
+                    'start_time' => 'required|date',
+                    'end_time' => 'required|date|after:start_date',
+                ]),
+                'user_id' => 3,
+            ]
+        );
+        return new EventResource($event);
     }
 
     /**
@@ -30,22 +43,36 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user');
+        return new EventResource($event);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update($request->validate(
+            [
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_time'
+            ]
+        ));
+        return new EventResource($event);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+        return response()->json(
+            [
+                'message' => 'delete event successfully!'
+            ]
+        );
     }
 }
